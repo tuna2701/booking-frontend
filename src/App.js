df5d3fe1429e7bd8df278fe0d075e4f81be358b5
tuna2701 <108262239+tuna2701@users.noreply.github.com>
2022-07-28 23:12:45 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import LoadingSpinner from "./components/loading/LoadingSpinner";
+// import logo from './logo.svg';
 import './App.css';
+import '.'
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  
+  // const componentDidMount = () => {
+  //   window.addEventListener('load', <LoadingSpinner />);
+  // }
+
+  const handleFetch = () => {
+    setIsLoading(true);
+    fetch("https://reqres.in/api/users?page=0")
+      .then((respose) => respose.json())
+      .then((respose) => {
+         setUsers(respose.data)
+         setIsLoading(false)
+        //  Optional code to simulate delay
+        //  setTimeout(() => {
+        //    setUsers(respose.data);
+        //    setIsLoading(false);
+        //  }, 3000);
+      })
+      .catch(() => {
+         setErrorMessage("Unable to fetch user list");
+         setIsLoading(false);
+      });
+  };
+  const renderUser = (
+    <div className="userlist-container">
+      {users.map((item, index) => (
+        <div className="user-container" key={index}>
+          <img src={item.avatar} alt="" />
+          <div className="userDetail">
+            <div className="first-name">{`${item.first_name}                
+                                   ${item.last_name}`}</div>
+            <div className="last-name">{item.email}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* {isLoading ? <div id="loader"></div> : <LoadingSpinner />} */}
+      
+      {isLoading ? <LoadingSpinner /> : renderUser}
+      {errorMessage && <div className="error">{errorMessage}</div>}
+      <button onClick={handleFetch} disabled={isLoading}>
+        Fetch Users
+      </button>
     </div>
   );
 }
